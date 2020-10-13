4fe74ce02ae5766cb9ffdfb62ec9ee8082fc6fd3
Jefferson Clark <jefferson.clark@idealtrends.com.br>
2020-10-13 10:16:31 -0300

    Admin -categorias

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ use \Slim\Slim;
 use \Hcode\PageAdmin;
 use \Hcode\Page;
 use \Hcode\Model\User;
+use \Hcode\Model\Categories;
 
 $app = new Slim();
 
@@ -223,6 +224,93 @@ $app->post("/admin/forgot/reset", function(){
     ]);
 
     $page->setTpl("forgot-reset-success");
+
+});
+
+$app->get("/admin/categories", function(){
+
+    User::verifyLogin();
+
+    $categories = Categories::listAll();
+
+    $page = new PageAdmin();
+
+    $page->setTpl("categories",[
+        "categories"=>$categories
+    ]);
+
+});
+
+$app->get("/admin/categories/create", function(){
+
+    User::verifyLogin();
+
+    $page = new PageAdmin();
+
+    $page->setTpl("categories-create");
+
+});
+
+$app->post("/admin/categories/create", function(){
+
+    User::verifyLogin();
+
+    $category = new Categories();
+
+    $category->setData($_POST);
+
+    $category->save();
+
+    header("Location: /admin/categories");
+    exit();
+
+});
+
+$app->get("/admin/categories/:idcategory/delete", function($idcategory){
+
+    User::verifyLogin();
+
+    $category = new Categories();
+
+    $category->get((int)$idcategory);
+
+    $category->delete();
+
+    header("Location: /admin/categories");
+    exit();
+
+});
+
+$app->get("/admin/categories/:idcategory", function($idcategory){
+
+    User::verifyLogin();
+
+    $category = new Categories();
+
+    $category->get((int)$idcategory);
+
+    $page = new PageAdmin();
+
+    $page->setTpl("categories-update",[
+        "category"=>$category->getValues()
+    ]);
+
+});
+
+$app->post("/admin/categories/:idcategory", function($idcategory){
+
+    User::verifyLogin();
+
+    $category = new Categories();
+
+    $category->get((int)$idcategory);
+
+    $category->setData($_POST);
+
+    $category->save();
+
+    header("Location: /admin/categories");
+    exit();
 
 });
 
