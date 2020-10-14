496dae338476b22074bcc4c844a72e2ea8ad33b0
Jefferson Clark <jefferson.clark@idealtrends.com.br>
2020-10-14 10:37:49 -0300

    Admin - Produtos

---
+++ admin-categories.php
@@ -0,0 +1,108 @@
+<?php
+
+use Hcode\Model\Categories;
+use Hcode\Model\User;
+use Hcode\Page;
+use Hcode\PageAdmin;
+
+$app->get("/admin/categories", function(){
+
+    User::verifyLogin();
+
+    $categories = Categories::listAll();
+
+    $page = new PageAdmin();
+
+    $page->setTpl("categories",[
+        "categories"=>$categories
+    ]);
+
+});
+
+$app->get("/admin/categories/create", function(){
+
+    User::verifyLogin();
+
+    $page = new PageAdmin();
+
+    $page->setTpl("categories-create");
+
+});
+
+$app->post("/admin/categories/create", function(){
+
+    User::verifyLogin();
+
+    $category = new Categories();
+
+    $category->setData($_POST);
+
+    $category->save();
+
+    header("Location: /admin/categories");
+    exit();
+
+});
+
+$app->get("/admin/categories/:idcategory/delete", function($idcategory){
+
+    User::verifyLogin();
+
+    $category = new Categories();
+
+    $category->get((int)$idcategory);
+
+    $category->delete();
+
+    header("Location: /admin/categories");
+    exit();
+
+});
+
+$app->get("/admin/categories/:idcategory", function($idcategory){
+
+    User::verifyLogin();
+
+    $category = new Categories();
+
+    $category->get((int)$idcategory);
+
+    $page = new PageAdmin();
+
+    $page->setTpl("categories-update",[
+        "category"=>$category->getValues()
+    ]);
+
+});
+
+$app->post("/admin/categories/:idcategory", function($idcategory){
+
+    User::verifyLogin();
+
+    $category = new Categories();
+
+    $category->get((int)$idcategory);
+
+    $category->setData($_POST);
+
+    $category->save();
+
+    header("Location: /admin/categories");
+    exit();
+
+});
+
+$app->get("/categories/:idcategory", function($idcategory){
+
+    $category = new Categories();
+
+    $category->get((int)$idcategory);
+
+    $page = new Page();
+
+    $page->setTpl("category",[
+        "category"=>$category->getValues(),
+        "products"=>[]
+    ]);
+
+});
